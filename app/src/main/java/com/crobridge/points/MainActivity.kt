682bd4268638bf7d945b4d1f97ae0a8ed3147dd6
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2023-06-19 00:59:17 +0200

    overflow menu, AlertDialog

--- app/src/main/java/com/crobridge/points/MainActivity.kt
+++ app/src/main/java/com/crobridge/points/MainActivity.kt
@@ -7,10 +7,13 @@ import com.google.android.material.tabs.TabLayout
 import androidx.viewpager2.widget.ViewPager2
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import com.crobridge.points.ui.main.SectionsPagerAdapter
 import com.crobridge.points.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.appcompat.app.AlertDialog;
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.myToolbar)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this)
         val viewPager: ViewPager2 = binding.viewPager
@@ -38,5 +42,41 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_game -> {
+                newGame()
+                true
+            }
+            R.id.load_game -> {
+                loadGame()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadGame() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Games")
+
+        val animals = arrayOf("one", "two", "three", "four", "five")
+        builder.setItems(animals) { dialog, index ->
+            // curent_game_id = index
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun newGame(){
+
     }
 }
